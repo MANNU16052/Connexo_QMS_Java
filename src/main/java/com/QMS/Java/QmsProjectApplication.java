c7c813eee879493a42bef8a2422f8e2ff742dd3e
manish <mannumalviya16052@gmail.com>
2024-01-21 22:44:25 +0530

    QMS Project Setup

---
+++ src/main/java/com/QMS/Java/QmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.QMS.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QMS/Java/QmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QMS.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
